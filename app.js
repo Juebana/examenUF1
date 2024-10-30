3ccba19bf2a386d73a81f5a684230a0137d52106
Guanlin Wang <guanlin987@gmail.com>
2024-10-30 20:19:23 +0100

    20:19

--- app.js
+++ app.js
@@ -7,11 +7,25 @@ function llegirProductes() {
     try {
         const data = fs.readFileSync('productes.json', 'utf-8');
         productesOBJ = JSON.parse(data);
+        return productesOBJ;
     } catch (error) {
         console.error('S’ha produït un error en llegir el fitxer:', error.message);
         return [];
     }
 }
 
-// llegirProductes();
+llegirProductes();
 // console.log(productesOBJ);
+
+// Tasca 2
+let producte = productesOBJ[0];
+
+function mostrarProducte(producte) {
+    console.log('Nom: ' + producte.nom);
+    console.log('Marca: ' + producte.marca);
+    console.log('Preu: ' + producte.preu);
+    console.log('Disponibilitat: ' + producte.disponible);
+    console.log('Caracteristiques: ' + producte.caracteristiques);
+}
+
+mostrarProducte(producte);
